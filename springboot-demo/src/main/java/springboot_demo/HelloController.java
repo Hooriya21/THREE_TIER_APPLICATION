1c3eb9416b8011096f98f56b0898da707af43051
Hooriya21 <hooriyanaaz370@gmail.com.com>
2026-07-14 06:58:04 +0530

    Track Spring Boot backend as regular folder

---
+++ springboot-demo/src/main/java/springboot_demo/HelloController.java
@@ -0,0 +1,13 @@
+package springboot_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from GitHub Actions CI/CD Pipeline!";
+    }
+}
